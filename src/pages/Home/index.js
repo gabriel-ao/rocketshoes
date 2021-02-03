1b607d83e29295899548906a00e166fb4b9243d5
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2021-02-03 11:25:38 -0300

    Subindo atualizações do projeto

---
+++ src/pages/Home/index.js
@@ -0,0 +1,124 @@
+import React from 'react';
+
+import { MdAddShoppingCart } from  'react-icons/md';
+import { ProductList } from './styles';
+
+function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/371570/25/sv01/fnd/BRA/w/320/h/320/bg/255,255,255"
+          alt="Tênis"
+        />
+        <strong> Tênis muito legal </strong>
+        <span> R$129,90 </span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  )
+}
+
+export default Home;
